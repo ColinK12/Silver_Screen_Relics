6e3b0bb5915c52b775de14706aa5fad4d9882641
Colin Koestler <KoestlerColinR@gmail.com>
2021-11-14 16:05:48 -0500

    Site/App is breathing again.  Scaffolding completed.

---
+++ SilverScreenRelics.Data/Entities/ArtItem.cs
@@ -0,0 +1,38 @@
+﻿using ScreenRelics.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SilverScreenRelics.Data.Entities
+{
+    public class ArtItem
+    {
+        [Key]
+        public int ArtItemId { get; set; } //Primary Key
+        [Required]
+        public string ArtItemTitle { get; set; }
+        [Required]
+        public string ArtItemDescription { get; set; }
+        [Required]
+        public double ArtItemPrice { get; set; }
+
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+
+        [ForeignKey("RelicTypes")]
+        public int RelicId { get; set; }//Composite Key/
+        public virtual RelicType RelicTypes { get; set; }
+        public virtual ICollection<ArtItem> ArtItems { get; set; }
+
+        [Required]
+        public bool? IsActive { get; set; }
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
--- SilverScreenRelics.Data/Properties/IdentityModels.cs
+++ SilverScreenRelics.Data/Properties/IdentityModels.cs
@@ -1,4 +1,6 @@
 ﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.Entity.ModelConfiguration;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -16,6 +18,8 @@ namespace ScreenRelics.Data
             // Add custom user claims here
             return userIdentity;
         }
+        public virtual ICollection<ArtListings> ArtListings { get; set; } = new List<ArtListings>();
+        public virtual ICollection<RelicType> RelicType { get; set; } = new List<RelicType>();
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
@@ -28,6 +32,37 @@ namespace ScreenRelics.Data
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+        public DbSet<ArtItem> ArtItemsSell { get; set; }
+        public DbSet<RelicType> RelicType { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<ArtListings> Art_Listings { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Conventions
+                .Remove<PluralizingTableNameConvention>();
+
+            modelBuilder
+                .Configurations
+                .Add(new IdentityUserLoginConfiguration())
+                .Add(new IdentityUserRoleConfiguration());
+        }
+    }
+    public class IdentityUserLoginConfiguration : EntityTypeConfiguration<IdentityUserLogin>
+    {
+        public IdentityUserLoginConfiguration()
+        {
+            HasKey(iul => iul.UserId);
+        }
+    }
+    public class IdentityUserRoleConfiguration : EntityTypeConfiguration<IdentityUserRole>
+    {
+        public IdentityUserRoleConfiguration()
+        {
+            HasKey(iur => iur.UserId);
         }
     }
 }
