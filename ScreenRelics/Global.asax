1e9f6c32be31ad35ed50c0f3f29c6ef21b4043df
Colin Koestler <KoestlerColinR@gmail.com>
2021-11-14 15:40:30 -0500

    Add project files.

---
+++ ScreenRelics/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ScreenRelics.MvcApplication" Language="C#" %>
---
+++ ScreenRelics/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ScreenRelics.Startup))]
+namespace ScreenRelics
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
